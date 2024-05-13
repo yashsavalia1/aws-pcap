ad97f96611626f9614deb9e927c96d03c6ce3826
Yash Savalia <savalia2@illinois.edu>
2024-05-13 17:42:35 -0400

    delete old dashboard

--- dashboard/packet_capture/netcap_upload.h
+++
@@ -1,8 +0,0 @@
-#ifndef NETCAP_UPLOAD_H
-#define NETCAP_UPLOAD_H
-#include "netparse.h"
-#include <pcap/pcap.h>
-
-void packet_handler(u_char *fileDumper, const struct pcap_pkthdr *header,
-                    const u_char *body);
-#endif
