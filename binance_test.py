e2a334500a2b00d596acd4bd7d658a964b1e98c6
Yash Savalia <savalia2@illinois.edu>
2024-06-11 18:18:01 -0400

    binance stream update w/ file watching

--- binance_test.py
+++ binance_test.py
@@ -1,33 +1,51 @@
 import asyncio
+import os
+import threading
+import time
 import websockets
 import json
+import requests
+
+os.environ['SSLKEYLOGFILE'] = 'keylog.txt'
+monitor_ip = "172.31.23.121"
+
+file_contents = ""
+with open("keylog.txt", "r") as f:
+    file_contents = f.read()
+
+stop_thread = False
+class FileWatcherThread(threading.Thread):
+    def run(self,*args,**kwargs):
+        global file_contents
+        global stop_thread
+        global monitor_ip
+        while True:
+            if stop_thread:
+                break
+            time.sleep(0.1)            
+            with open("keylog.txt", "r") as f:
+                c = f.read()
+                if c != file_contents:
+                    requests.post(f"http://{monitor_ip}/api/ssl-keys", files={"file": open("keylog.txt", "rb")})
+                    
+
+t = FileWatcherThread()
+t.start()
 
 async def subscribe(symbol):
-    uri = "wss://testnet.binance.vision/ws-api/v3"
+    uri = "wss://fstream.binance.com/ws/btcusdt@aggTrade"
     
     async with websockets.connect(uri) as websocket:
-        # Subscribing to the depth stream
-        depth_params = {
-            "id": 1,
-            "method": "SUBSCRIBE",
-            "params": [f"{symbol}@depth"]
-        }
-        await websocket.send(json.dumps(depth_params))
-
-        # Subscribing to the trade stream
-        trade_params = {
-            "id": 2,
-            "method": "SUBSCRIBE",
-            "params": [f"{symbol}@trade"]
-        }
-        await websocket.send(json.dumps(trade_params))
-
         # Handle incoming messages
         while True:
             response = await websocket.recv()
             data = json.loads(response)
             print(data)
 
+
 # Run the client
 symbol = "bnbbtc"
-asyncio.get_event_loop().run_until_complete(subscribe(symbol))
+try:
+    asyncio.get_event_loop().run_until_complete(subscribe(symbol))
+except KeyboardInterrupt:
+    stop_thread = True
